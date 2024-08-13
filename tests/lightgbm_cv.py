e508f53e1641f144f83bb4377e17877534e53b1f
liezeltamon <lizheltamon@gmail.com>
2024-08-13 18:10:32 +0100

    Add plots for assessing optimisation

--- tests/lightgbm_cv.py
+++ tests/lightgbm_cv.py
@@ -1,24 +1,62 @@
-# Use xgboost_cv.py as reference - https://github.com/optuna/optuna-examples/blob/main/xgboost/xgboost_cv.py
+# LightGBM CV + Optuna for tabular data
+
+#%%
 
 import lightgbm as lgb
+import matplotlib.pyplot as plt
+import numpy as np
 import optuna
-from sklearn.datasets import load_breast_cancer
+import pandas as pd
+from plotnine import ggplot, geom_point, aes, theme_classic, labs
+from sklearn.datasets import load_breast_cancer, load_diabetes
+import joblib, json, yaml
 
+#%%
 # Parameters
-eval_metric = "auc"
-seed_value = int(290)
-opt_direction = "maximize"
-opt_n_trials = int(50)
 
-def objective(trial):
+with open("config.yml", "r") as file:
+    config = yaml.safe_load(file)
+
+random_state = int(config["general"]["random_state"])
+eval_metric = config["general"]["eval_metric"]
+objective_type = config["general"]["objective_type"]
+opt_direction = config["optimise"]["opt_direction"]
+opt_n_trials = int(config["optimise"]["opt_n_trials"])
+plot_n_trials = config["plot"]["plot_n_trials"] # If None, plot all trials
+num_cores = int(config["general"]["num_cores"])
+cv_nfold = int(config["cross_val"]["cv_nfold"])
+cv_num_boost_round = int(config["cross_val"]["cv_num_boost_round"])
+test = config["general"]["test"]
+
+#%%
+# Load data
+if test and objective_type in ["binary", "multiclass"]:
     data, target = load_breast_cancer(return_X_y=True)
+elif test and objective_type == "regression":
+    data, target = load_diabetes(return_X_y=True)
+elif not test:
+    data = pd.read_csv("data.csv")
+    target = pd.read_csv("target.csv")
+else:
+    raise ValueError("Problem with data loading")
+#%%
+# **Preprocessing**
+
+#%%
+# Optimise hyperparameters
+
+def objective(trial, data=data, target=target, objective_type=objective_type, eval_metric=eval_metric, cv_num_boost_round=cv_num_boost_round, cv_nfold=cv_nfold):
+
+    # Should be inside the objective function to not get this error:
+    # LightGBMError: Reducing `min_data_in_leaf` with `feature_pre_filter=true` may cause unexpected behaviour for features that were pre-filtered by the larger `min_data_in_leaf`.
     dtrain = lgb.Dataset(data, label=target)
 
     param = {
-        "objective": "binary",
+        "objective": objective_type,
         "metric": eval_metric,
         "verbosity": -1,
         "boosting_type": "gbdt",
+        "nfold": trial.suggest_int("nfold", 3, 10),
         "lambda_l1": trial.suggest_float("lambda_l1", 1e-8, 10.0, log=True),
         "lambda_l2": trial.suggest_float("lambda_l2", 1e-8, 10.0, log=True),
         "num_leaves": trial.suggest_int("num_leaves", 2, 256),
@@ -28,91 +66,113 @@ def objective(trial):
         "min_child_samples": trial.suggest_int("min_child_samples", 5, 100),
     }
 
-    cv_results = lgb.cv(
+    trial_results = lgb.cv(
         param,
         dtrain,
-        num_boost_round=100,
-        nfold=3,
+        num_boost_round=cv_num_boost_round,
+        #nfold=cv_nfold,
         stratified=True,
         shuffle=True,
         metrics=eval_metric,
-        seed=seed_value,
+        seed=trial.number,
+        eval_train_metric=True,
+        return_cvbooster=True,
     )
 
-    return max(cv_results["valid " + eval_metric + "-mean"])
+    trial.set_user_attr("trial_results", trial_results)
+    
+    # Save the trial results and parameters
+    joblib.dump(trial_results, f"trial_results_{trial.number}.pkl")
+    with open(f"trial_params_{trial.number}.json", "w") as f:
+        json.dump(trial_results["cvbooster"].boosters[0].params, f, indent=4)
+    
+    return max(trial_results["valid " + eval_metric + "-mean"])
 #
-
-if __name__ == "__main__":
-    study = optuna.create_study(direction=opt_direction)
-    study.optimize(objective, n_trials=opt_n_trials)
-    #study.optimize(objective, n_trials=opt_n_trials, callbacks=[TerminatorCallback()])
-
-    print(f"The number of trials: {len(study.trials)}")
-    print(f"Best value: {study.best_value} (params: {study.best_params})")
-
+#%%
+# if __name__ == "__main__":
+study = optuna.create_study(direction=opt_direction) # optuna.create_study(pruner=None) by default i.e. MedianPruner is used
+study.optimize(objective, n_trials=opt_n_trials, n_jobs = num_cores)
 ############################################################################################################
+#%%
+# Assess whether you need to repeat optimisation or not
 
-# data, target = sklearn.datasets.load_breast_cancer(return_X_y=True)
-# dtrain = lgb.Dataset(data, label=target)
+study_df = study.trials_dataframe()
+study_df.sort_values("value", ascending=False, inplace=True)
+study_df.to_csv("trials_study.csv", index=False)
+study_df["number"] = pd.Categorical(study_df["number"], categories=study_df['number'].unique(), ordered=True)
+study_df.head()
 
-# # param = {
-# #     "objective": "binary",
-# #     "metric": eval_metric,
-# #     "verbosity": -1,
-# #     "boosting_type": "gbdt",
-# #     "lambda_l1": trial.suggest_float("lambda_l1", 1e-8, 10.0, log=True),
-# #     "lambda_l2": trial.suggest_float("lambda_l2", 1e-8, 10.0, log=True),
-# #     "num_leaves": trial.suggest_int("num_leaves", 2, 256),
-# #     "feature_fraction": trial.suggest_float("feature_fraction", 0.4, 1.0),
-# #     "bagging_fraction": trial.suggest_float("bagging_fraction", 0.4, 1.0),
-# #     "bagging_freq": trial.suggest_int("bagging_freq", 1, 7),
-# #     "min_child_samples": trial.suggest_int("min_child_samples", 5, 100),
-# # }
+#%%
+### 1) Plot showing rank of trials
 
-# params = {
-#     'objective': 'binary',              # Binary classification
-#     'metric': 'auc',                    # Evaluation metric
-#     'boosting_type': 'gbdt',            # Gradient Boosting Decision Tree
-#     'num_leaves': 31,                   # Number of leaves in one tree
-#     'learning_rate': 0.05,              # Learning rate
-#     'feature_fraction': 0.9             # Fraction of features used for each tree
-# }
+p = (
+    ggplot(study_df, aes(x="number", y="value"))
+    + geom_point()
+    + labs(y = "Optimisation value", x = "Trial number")
+    + theme_classic()
+)
+p.save("trials_rank.png", dpi=300)
 
-# # Perform cross-validation
-# cv_results = lgb.cv(
-#     params,
-#     dtrain,
-#     num_boost_round = 100,
-#     nfold=3,
-#     stratified=True,
-#     shuffle=True,
-#     metrics='auc',
-#     seed = seed_value,
-#     eval_train_metric=True,
-#     return_cvbooster=True
-# )
+#%%
+#### 2) Plot validation and training metrics across iterations to compare best and worst trials
+if plot_n_trials is not None:
+    plot_trials_inds = np.linspace(0, study_df.shape[0] - 1, num=int(plot_n_trials), endpoint=True, dtype=int)
+    study_df = study_df.iloc[plot_trials_inds, :]
 
-# # Get length of values in a dictionary for each key
-# for key, value in cv_results.items():
-#     print(len(value))
+num_cols = min(study_df.shape[0], 3)
+num_rows = np.ceil(study_df.shape[0] / num_cols).astype(int)
+fig, axes = plt.subplots(num_rows, num_cols, figsize=(5*num_cols, 5*num_rows))
 
-# cv_boosters = cv_results['cvbooster']
-# #cv_boosters.save_model('model.txt')
-# print(f'Number of Boosters: {len(cv_boosters.boosters)}')
+# Get range of objective value for make y-axis consistent across all plots
+minmax_array = np.full((study_df.shape[0], 2), None)
+for i, trial_number in enumerate(study_df["number"]):
+    trial_results = study.trials[int(trial_number)].user_attrs["trial_results"]
+    all_metrics = trial_results["train " + eval_metric + "-mean"] + trial_results["valid " + eval_metric + "-mean"]
+    minmax_array[i,0] = min(all_metrics)
+    minmax_array[i,1] = max(all_metrics)
+min_eval_metric = np.min(minmax_array)
+max_eval_metric = np.max(minmax_array)
 
-# # booster methods in - https://lightgbm.readthedocs.io/en/latest/pythonapi/lightgbm.Booster.html#lightgbm.Booster
-# booster_fold = cv_boosters.boosters[0]
-# booster_fold.feature_name() # List of feature names
-# booster_fold.feature_importance() # Numpy array of feature importance integers
-# booster_fold.current_iteration()
-# booster_fold.eval_train() # Dictionary of evaluation results on training data
-# booster_fold.eval_valid() # Dictionary of evaluation results on validation data
+#%%
+for i, trial_number in enumerate(study_df["number"]):
+    #trial_results = joblib.load(f"trial_results_{trial_number}.pkl")
+    trial_results = study.trials[int(trial_number)].user_attrs["trial_results"]
+    train_metrics = trial_results["train " + eval_metric + "-mean"]
+    valid_metrics = trial_results["valid " + eval_metric + "-mean"]
+    iter_nums = range(0, len(train_metrics))
+    
+    row_idx = i // num_cols
+    col_idx = i % num_cols
+    axes[row_idx, col_idx].plot(iter_nums, train_metrics, color="turquoise", label="Train")
+    axes[row_idx, col_idx].plot(iter_nums, valid_metrics, color="darkviolet", label="Validation")
+    axes[row_idx, col_idx].set_xlabel("Iteration")
+    axes[row_idx, col_idx].set_ylabel(f"Objective value: {eval_metric}")
+    axes[row_idx, col_idx].set_title(f"Trial {trial_number}")
+    axes[row_idx, col_idx].legend()
+    axes[row_idx, col_idx].set_xlim(0, cv_num_boost_round)
+    axes[row_idx, col_idx].set_ylim(min_eval_metric - (0.1 * min_eval_metric), max_eval_metric + (0.1 * max_eval_metric))
+fig.tight_layout()
+fig.savefig("trials_metrics.png", dpi=300)
+plt.show()
 
-# # Can be used to get train and valid metrics per iteration
-# booster_fold.rollback_one_iter()
-# booster_fold.current_iteration()
-# booster_fold.eval_train()
-# booster_fold.eval_valid()
+#%%
+# **Further explore study**
+# Most important hyperparameters, performance for each range of heperparameters, etc.
+# See https://optuna.readthedocs.io/en/stable/tutorial/10_key_features/005_visualization.html#visualization
+# and https://optuna.readthedocs.io/en/stable/reference/visualization/matplotlib.html
 
-# # Return the best score (maximum auc)
-# #return max(cv_results['auc-mean'])
+#from optuna.visualization.matplotlib import plot_param_importances
+from optuna.visualization import plot_optimization_history
+from optuna.visualization import plot_param_importances
+plot_optimization_history(study)
+plot_param_importances(study)
+
+#%% 
+# Explore feature importance
+trial_number = 2
+trial_results = joblib.load(f"trial_results_{trial_number}.pkl")
+trial_cvbooster = trial_results["cvbooster"]
+trial_cvbooster.feature_importance(importance_type="gain")
+trial_cvbooster.feature_importance(importance_type="split")
+
+# %%
