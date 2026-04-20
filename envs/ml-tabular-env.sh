9137aac6214e224a05b81528f56482f910a954f4
Liezel Tamon <yfg436@cluster2.hpc.in.bmrc.ox.ac.uk>
2026-04-20 14:37:31 +0100

    Initial commit: Explain model with shap

--- envs/ml-tabular-env.sh
+++ envs/ml-tabular-env.sh
@@ -7,6 +7,7 @@ mamba create -y -n "${ENV_NAME}" python=3.12
 mamba run -n "${ENV_NAME}" pip install "flaml[automl]"
 mamba run -n "${ENV_NAME}" pip install lazypredict[boost]
 mamba run -n "${ENV_NAME}" pip install mlflow optuna
+mamba run -n "${ENV_NAME}" pip install shap
 
 printf "\nEnvironment created. Activate it with:\n"
 printf "mamba activate %s\n" "${ENV_NAME}"
